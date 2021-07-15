7c0c7e9b2756b823b6a872d455208384b3739172
NidaAP <smilykodi@gmail.com>
2021-07-15 22:04:57 +0530

    first commit

---
+++ src/main/java/com/gitopsdemo/gitopsdemocalcproject/App.java
@@ -0,0 +1,13 @@
+package com.gitopsdemo.gitopsdemocalcproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
